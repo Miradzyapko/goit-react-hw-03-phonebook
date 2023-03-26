9af72639cc9ef789ebd57cb15c203e26c75d2051
Miradzyapko <mira.dzyapko@gmail.com>
2023-03-26 23:09:53 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,20 @@ export class App extends Component {
       
       filter: ''
     };
+ componentDidMount() {
+const contacts = localStorage.getItem('contacts');
+const parsedContacts = JSON.parse(contacts);
+if (parsedContacts !== null) {
+  this.setState({ contacts: parsedContacts }) 
+}
+console.log(parsedContacts)
+ }
+ componentDidUpdate(prevProps, prevState) {
+  if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem ('contacts', JSON.stringify(this.state.contacts))
+  }
  
+ }
     FormSubmitHandler = ({ name, number }) => {
       const contact = {
         id: nanoid(),
