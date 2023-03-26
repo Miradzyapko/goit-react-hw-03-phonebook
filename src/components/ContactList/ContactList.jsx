2026acf14a6611d825ace050db2203b187207b4a
Miradzyapko <mira.dzyapko@gmail.com>
2023-03-26 23:47:41 +0300

    bug fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
-import React from "react";
-import { List, Item, Button } from './ContactList.Styled'; 
-import { ListItem } from 'components/ContactListItem/ContactListItem.Styled';
+import { List, ListItem, Item} from './ContactList.Styled'; 
+import { Button } from './ContactList.Styled';
+
 export const ContactList = ({contacts, onDelete }) => {
     return (
         <List>   
@@ -13,7 +13,7 @@ export const ContactList = ({contacts, onDelete }) => {
         ))}
     </List>
 )
-        }       
+        }
 ContactList.propTypes = {
     onDelete: PropTypes.func.isRequired,
     contacts: PropTypes.arrayOf(PropTypes.exact({
